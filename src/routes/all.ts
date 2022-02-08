5aaa728722e9b71871ffd301819b9d82d1ba3308
brechtvdv <br5cht@hotmail.com>
2022-02-08 14:19:44 +0100

    Add path to redirect of /

--- src/routes/all.ts
+++ src/routes/all.ts
@@ -2,14 +2,15 @@
 import { getDcatPage } from "../controllers/dcatController.js";
 import { getHierarchicalPage } from "../controllers/hierarchicalViewController.js";
 import { getPaginatedPage } from "../controllers/paginatedViewController.js";
+import {getConfig} from "../config/config";
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.redirect('/dcat');
+    res.redirect(getConfig().targetURI + '/dcat');
 });
 router.get('/dataset', (req, res) => {
-    res.redirect('/dcat');
+    res.redirect(getConfig().targetURI + '/dcat');
 });
 
 router.get("/dcat", getDcatPage);
