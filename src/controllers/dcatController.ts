bb507983e72c32ac2866b81f4ed3c77045fd09f1
brechtvdv <br5cht@hotmail.com>
2022-08-31 14:53:53 +0200

    Use DCAT Services and fetch types to list LDESs

--- src/controllers/dcatController.ts
+++ src/controllers/dcatController.ts
@@ -1,5 +1,11 @@
 import { getConfig } from "../config/config.js";
-import {HttpHandler, HttpHandlerInput, HttpRequest, HttpResponse} from "@solid/community-server";
+import {
+    encodeUriPathComponents,
+    HttpHandler,
+    HttpHandlerInput,
+    HttpRequest,
+    HttpResponse
+} from "@solid/community-server";
 import {HierarchicalViewControllerArgs} from "./hierarchicalViewController";
 import {Fetcher} from "../utils/Fetcher";
 
@@ -8,14 +14,20 @@ export interface DcatControllerArgs {
      * Base URL of the server
      */
     baseUrl: string;
+    /**
+     * Fetcher to use
+     */
+    fetcher: Fetcher;
 }
 
 export class DcatController extends HttpHandler {
     private readonly baseUrl: string;
+    private readonly fetcher: Fetcher;
 
     public constructor(args: DcatControllerArgs) {
         super();
         this.baseUrl = args.baseUrl;
+        this.fetcher = args.fetcher;
     }
 
     handle(input: HttpHandlerInput): Promise<void> {
@@ -27,7 +39,7 @@ export class DcatController extends HttpHandler {
 
     async getDcatPage(req: HttpRequest, res: HttpResponse) {
         const md: any = {
-            "@context": ["https://apidg.gent.be/opendata/adlib2eventstream/v1/context/DCAT-AP-VL.jsonld", {
+            "@context": ["https://data.vlaanderen.be/doc/applicatieprofiel/DCAT-AP-VL/erkendestandaard/2021-12-02/context/DCAT-AP-VL-20.jsonld", {
                 "dcterms": "http://purl.org/dc/terms/",
                 "ldes": "https://w3id.org/ldes#",
                 "tree": "https://w3id.org/tree#",
@@ -49,15 +61,17 @@ export class DcatController extends HttpHandler {
                 "@id": "https://creativecommons.org/publicdomain/zero/1.0/"
             },
             "Datasetcatalogus.heeftDataset": [],
+            "Datasetcatalogus.heeftService": []
         };
 
-        const types = getConfig().types;
-        console.log(types);
+        const types = await this.getTypes();
         for (const type of types) {
-            const encodedType: string = encodeURIComponent(type);
-            const hierarchicalView: string = this.baseUrl + "hierarchical?type=" + encodedType;
-            const datasetURI: string = this.baseUrl + "dataset?type=" + encodedType;
-            const beschrijving: string = `Event Stream van entiteiten van het type: ${type}`;
+            const expandedType = type.id;
+            if (expandedType.indexOf('ngsi-ld:') !== -1) expandedType.replace('ngsi-ld:', 'https://uri.etsi.org/ngsi-ld/');
+            const encodedExpandedType: string = encodeURIComponent(expandedType);
+            const hierarchicalView: string = this.baseUrl + "hierarchical?type=" + encodedExpandedType;
+            const datasetURI: string = this.baseUrl + "dataset?type=" + encodedExpandedType;
+            const beschrijving: string = `Event Stream van entiteiten van het type: ${expandedType}`;
             const dataset = {
                 "@id": datasetURI,
                 "@type": [
@@ -73,22 +87,50 @@ export class DcatController extends HttpHandler {
                     "@value": beschrijving,
                     "@language": "nl",
                 },
-                "Dataset.toegankelijkheid": "http://publications.europa.eu/resource/authority/access-right/PUBLIC",
-                "heeftDistributie": {
-                    "@type": "Distributie",
-                    "toegangsURL": hierarchicalView,
-                    "dcterms: conformsTo": "https://w3id.org/tree",
-                    "Distributie.heeftLicentie": {
-                        "@id": "https://creativecommons.org/publicdomain/zero/1.0/"
-                    },
-                },
+                "Dataset.toegankelijkheid": "http://publications.europa.eu/resource/authority/access-right/PUBLIC"
             };
             md["Datasetcatalogus.heeftDataset"].push(dataset);
+
+            const service = {
+                "@type": ["ldes:EventSource", "Dataservice"],
+                "Dataservice.biedtInformatieAanOver": datasetURI,
+                "Dataservice.endpointUrl": hierarchicalView,
+                "Dataservice.conformAanProtocol": "https://w3id.org/ldes",
+                "Dataservice.licentie": {
+                    "@id": "https://creativecommons.org/publicdomain/zero/1.0/"
+                }
+            };
+
+            md["Datasetcatalogus.heeftService"].push(service);
         }
 
         res.setHeader("Content-Type", "application/ld+json; charset=utf-8");
         res.setHeader("Cache-Control", `public, max-age=${60 * 60 * 24}`);
 
         res.end(JSON.stringify(md));
+    }
+
+    async getTypes(): Promise<any[]> {
+        const types = [];
+        const typesEndpoint = 'http://localhost:9090/ngsi-ld/v1/types';
+        const response = await this.fetcher.fetch(typesEndpoint);
+        const typesResponse = await response.json();
+
+        if (typesResponse.typeList) {
+            for (const type of typesResponse.typeList) {
+                const typeInfo = await this.fetcher.fetch(typesEndpoint + '/' + encodeURIComponent(type));
+                const typeInfoJson = await typeInfo.json();
+                types.push(typeInfoJson);
+            }
+        }
+        else {
+            // use config types when nothing found
+            for (const type of getConfig().types) {
+                types.push({
+                    "id": type
+                })
+            }
+        }
+        return types;
     }
 }
